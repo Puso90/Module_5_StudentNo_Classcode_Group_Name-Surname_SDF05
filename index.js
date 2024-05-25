4329815a0ec15455dde3d4805666622f75d16c22
Puso Ramoroa <43412934+Puso90@users.noreply.github.com>
2024-05-26 01:14:06 +0200

    Black Jack Game Project
    Coded a black jack game with Scrimba tutorial

--- index.js
+++ index.js
@@ -1,11 +1,113 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+//Black Jack Game 
+//let firstCard = getRandomCard() //Variables
+//let secondCard = getRandomCard() //Variables
+//Player objects with values
+let player = {
+    name: "Puso",
+    chips: 300 
+}
+let cards = [] //Variables | This was changed and added to the startGame Function
+let sum = 0 //Variables | By default the number on the sum of cards should be zero before you generate the cards
+let hasBlackJack = false //Booleans
+let isAlive = false //Booleans | Must be set to false(default) because you have not started game yet 
+let message = " "  //Empty String To Be Added At Functions
+//Do you want to draw card message
+let messageEl = document.getElementById("message-el") //Variables
+//Accumulated card number you have in game
+let sumEl = document.getElementById("sum-el") //Variables
+//Cards JavaScript selector variable
+let cardsEl = document.getElementById("cards-el") //Variables
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": R" + player.chips
 
-let sum = firstCard + secondCard
 
-console.log(sum)
+//Creating Randomness For Cards Drawn
+function getRandomCard() {
+    let randomNumber =  Math.floor( Math.random() * 13 ) + 1
+    if ( randomNumber > 10 ) {
+        return 10
+    } else if ( randomNumber === 1 ) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {   
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    //Added loop for displaying array - very important for me to remeber
+    for (let i = 0; i < cards.length; i ++) {
+        cardsEl.textContent += cards[i] + "  "
+    }
+    sumEl.textContent = "Sum:" + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card"
+    }   
+    else if (sum === 21) {
+        message = "You've got BlackJack!"
+        hasBlackJack = true
+    }
+    else  {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    if ( isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        //Pushed card to array
+        cards.push(card) 
+        renderGame()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+//Club Admission
+let age = 22 
+
+if (age < 21) {
+    console.log("You cannto enter club!")
+} 
+else {
+    console.log("Welcome!")
+}
+
+//Norway King Letter
+let age = 100
+
+if (age < 100) {
+    console.log("Not elegible")
+} 
+else if (age === 100) {
+    console.log("Here is your birthday card from the King!")
+}
+else {
+    console.log("Not elegible, you already got one!")
+}
+
+*/
